8de6529e6f30f37adf344689b8a43e694fea2fab
Roppon Picha <aeroppon@gmail.com>
2020-10-11 14:17:33 +0700

    revised code functions2.py with added description

--- functions2.py
+++ functions2.py
@@ -1,14 +1,18 @@
-def myfunc(mystring):
+def alternate_casing(mystring):
+    '''format a string with alternating cases'''
     n = len(mystring)
     l = list()
     num = 0
-    for letter in mystring:
-        
-        if num % 2 ==0:
+    for letter in mystring:        
+        if num % 2 == 0:
             l.append(letter.lower())
         else:
             l.append(letter.upper())
         num+=1
     st = str(l)
     return st
-myfunc('anthro')
+
+
+text = 'anthro'
+print(text)
+print(alternate_casing(text))
