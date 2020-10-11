e0858e0beab6d98df7b982f6017251045103694e
buddy8300 <atharva8300@gmail.com>
2020-10-11 17:46:42 +0530

    Altered Animal(inheriteance).py , by adding another class (cat) to demonstrate polymorphism too.

--- Animal(inheritance).py
+++ Animal(inheritance).py
@@ -9,5 +9,18 @@ class dog(animal):
     def __init__(self):
         animal.__init__(self)
         print("and i am a dog")
-myDog = dog()
-myDog.running()
+    def speak(self):
+        print("woof woof")
+class cat(animal):
+    def __init__(self):
+        animal.__init__(self)
+        print("and i am a cat")
+    def speak(self):
+        print("meow meow")
+
+dholu = dog()
+billu = cat()
+for x in (dholu,billu):
+    x.speak()
+
+#polymorphism and inheritence 
